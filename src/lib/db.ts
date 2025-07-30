510f74023d64c7f85a677d7aa16f2ee750d91cb8
Jorge Oliveira <dody60314@gmail.com>
2025-07-30 13:56:03 +0000

    Eu publiquei no onrender mas ele fica no carregando banco de dados, engr

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,4 +5,7 @@ import { Pool } from 'pg';
 // em todas as chamadas de função do servidor.
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
