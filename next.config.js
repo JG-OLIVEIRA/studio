6411928120ac9031f396ca74c386c4d68c482d14
Jorge Oliveira <dody60314@gmail.com>
2025-08-01 13:32:05 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer, dev }) => {
+    // Disable webpack cache in development to prevent issues with corrupted cache files.
+    if (dev) {
+      config.cache = false;
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
